060f7c3e8c0e2491d4ff889837460bae1ea6bf19
Archie Ruin <30303225+archieruin@users.noreply.github.com>
2019-11-09 14:12:53 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-TOKEN = "912590787:AAF_uuHBKhWC8fzr1Q-aPguqkf5KxOpHxlo"
-CHROME_DRIVER_PATH = os.path.abspath('chromedriver')
-mail = 'archieruin@gmail.com'
-password = '102938382910RuinArchie'
-cache_file = 'cache.txt'
-
-if __name__ == '__main__':
-    print(CHROME_DRIVER_PATH)
